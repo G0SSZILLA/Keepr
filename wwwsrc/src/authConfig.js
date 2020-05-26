cf6e6658f8cfe6c1915db36f5460b006128064ea
G0SSZILLA <gossman15@gmail.com>
2020-05-26 15:40:20 -0600

    Auth0 is connecting

--- wwwsrc/src/authConfig.js
+++ wwwsrc/src/authConfig.js
@@ -1,3 +1,3 @@
-export const domain = "";
-export const audience = "";
-export const clientId = "";
+export const domain = "gosszilla.auth0.com";
+export const audience = "https://keepr.com";
+export const clientId = "qUoxUv5eY38hWJBhFthwR5FqLl4W9wx1";
